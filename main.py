4f3182cde08c615ac055a3afd8dd4880abc0c12a
Lechatellier <2020001022@itpt.local>
2020-12-15 14:16:16 +0000

    another version

--- main.py
+++ main.py
@@ -2,11 +2,21 @@ import json
 data = json.load(open("data.json"))
 
 def translate(w):
-    
-    return data[w]
-
+    w = w.lower()
+    if w in data:
+        return data[w]
+    elif len(get_close_matches(w, data.keys())) > 0:
+        yn = input("Did you mean %s instead? Enter Y if yes, or N if no: " % get_close_matches(w, data.keys())[0])
+        #confirmation from user
+        if yn == "Y":
+            return data[get_close_matches(w, data.keys())[0]]
+        elif yn == "N":
+            return "The word doesn't exist. Please double check it."
+        else:
+            return "We didn't understand your entry."
+    else:
+        return "The word doesn't exist. Please double check it."
 word = input("Enter a word: ")
 
 print(translate(word))
-
-#problems if word does not exisits
+#two definations formatting e.g tree, cool
